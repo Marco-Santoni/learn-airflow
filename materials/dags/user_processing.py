9a5da3842748ddb6e5bf75bfc0d61d58558fc6ad
Marco Santoni <marco.santoni@flowe.com>
2024-08-16 07:06:23 +0200

    add first files

---
+++ materials/dags/user_processing.py
@@ -0,0 +1,57 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from pandas import json_normalize
+import json
+
+
+def _process_user(task_instance):
+    api_json = task_instance.xcom_pull(task_ids="extract_user")
+    first_user = api_json["results"][0]
+    df = json_normalize({
+        "firstname": first_user["name"]["first"]
+    })
+    df.to_csv("/tmp/processed_user.csv", index=None, header=False)
+
+def _store_user():
+    hook = PostgresHook(postgres_conn_id="postgres")
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER AS ',' ",
+        filename="/tmp/processed_user.csv"
+    )
+
+with DAG("marco_processing", schedule_interval="@daily", start_date=datetime(2024,8,12),  catchup=False) as dag:
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (
+            firstname TEXT NOT NULL)
+            """
+    )
+    http_sensor = HttpSensor(
+        task_id="is_api_available",
+        http_conn_id="user_api",
+        endpoint="api/"
+    )
+    extract_user = SimpleHttpOperator(
+        task_id="extract_user",
+        http_conn_id="user_api",
+        endpoint="api/",
+        method="GET",
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+    process_user = PythonOperator(
+        task_id="process_user",
+        python_callable=_process_user
+    )
+    store_user = PythonOperator(
+        task_id="store_user",
+        python_callable=_store_user
+    )
+    create_table >> http_sensor >> extract_user >> process_user >> store_user
